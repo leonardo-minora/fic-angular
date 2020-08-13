06a2d1a3926b08e3291305992ff3d36a2dee5d24
Leonardo Ataide MINORA <leonardo.minora@gmail.com>
2020-08-13 12:46:07 -0300

    added button action in tarefa component

--- README.md
+++ README.md
@@ -31,5 +31,5 @@
 - [X] Criar Input Field com two-way binding de  `nome`.
 - [X] Mostar o valor armazendo no atributo `nome` em um parágrafo usando string interpolation.
 - [X] Adicionar um botão que só pode estar habilitado se `nome` não for vazio.
-- [ ] Ao clicar no botão, o atributo `nome` deve ser reiniciado, recebendo uma string vazia.
-     - _Sugestão_: Mostrar mensagem informando que nome foi adicionar (com JS alert, por exemplo)
+- [X] Ao clicar no botão, o atributo `nome` deve ser reiniciado, recebendo uma string vazia.
+     - [X] _Sugestão_: Mostrar mensagem informando que nome foi adicionar (com JS alert, por exemplo)
--- src/app/components/tarefa/tarefa.component.ts
+++ src/app/components/tarefa/tarefa.component.ts
@@ -5,11 +5,35 @@ import { Component } from '@angular/core';
     templateUrl: './tarefa.component.html'
 })
 export class TarefaComponent {
+    mostraMensagem = false;
     nome: string  = 'Minha nova tarefa fixa';
+    tarefas = [];
 
     constructor() {}
 
     tarefaVazia() {
         return this.nome.length > 0;
+    }
+
+    onAdicionarTarefa() {
+        this.mostraMensagem = true;
+        setTimeout(
+            () => { this.mostraMensagem = false; },
+            2000
+        );
+
+        let tarefa = new Tarefa(this.tarefas.length+1, this.nome);
+        this.tarefas.push(tarefa);
+        this.nome = '';
+    }
+}
+
+class Tarefa {
+    id: number = 0;
+    titulo: string = '';
+
+    constructor(id: number, titulo: string) {
+        this.id = id;
+        this.titulo = titulo;
     }
 }
